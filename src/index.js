1820dabed393813c9431912b75fdc184d9f6a01a
Josh Choo <dev@joshuous.com>
2020-01-28 15:15:38 +0800

    generate drum pads by mapping from audio bank

--- src/index.js
+++ src/index.js
@@ -2,12 +2,76 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const audioBank = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+  },
+];
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audioBank: audioBank,
+    };
+  }
+
   render() {
     return (
       <div id="drum-machine">
         <Display />
-        <DrumPad />
+        <DrumPad audioBank={this.state.audioBank} />
       </div>
     );
   }
@@ -20,47 +84,24 @@ class Display extends React.Component {
 }
 
 class DrumPad extends React.Component {
+  createDrumPads(audioBank) {
+    return audioBank.map(element => {
+      const { keyCode, keyTrigger, id, url } = element; // keyCode still unused
+
+      return (
+        <div id={id} className="drum-pad">
+          <audio id={keyTrigger} class="clip" src={url}></audio>
+          <p>{keyTrigger}</p>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return (
-      <div id="pad-bank">
-        <div id="key1" className="drum-pad">
-          <audio id="Q" class="clip" src="#"></audio>
-          <p>Q</p>
-        </div>
-        <div id="key2" className="drum-pad">
-          <audio id="W" class="clip" src="#"></audio>
-          <p>W</p>
-        </div>
-        <div id="key3" className="drum-pad">
-          <audio id="E" class="clip" src="#"></audio>
-          <p>E</p>
-        </div>
-        <div id="key4" className="drum-pad">
-          <audio id="A" class="clip" src="#"></audio>
-          <p>A</p>
-        </div>
-        <div id="key5" className="drum-pad">
-          <audio id="S" class="clip" src="#"></audio>
-          <p>S</p>
-        </div>
-        <div id="key6" className="drum-pad">
-          <audio id="D" class="clip" src="#"></audio>
-          <p>D</p>
-        </div>
-        <div id="key7" className="drum-pad">
-          <audio id="Z" class="clip" src="#"></audio>
-          <p>Z</p>
-        </div>
-        <div id="key8" className="drum-pad">
-          <audio id="X" class="clip" src="#"></audio>
-          <p>X</p>
-        </div>
-        <div id="key9" className="drum-pad">
-          <audio id="C" class="clip" src="#"></audio>
-          <p>C</p>
-        </div>
-      </div>
-    );
+    const audioBank = this.props.audioBank;
+    const drumPads = this.createDrumPads(audioBank);
+
+    return <div id="pad-bank">{drumPads}</div>;
   }
 }
 

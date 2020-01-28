dc9646d6989103ac5299bc58e2cd055203fb5477
Josh Choo <dev@joshuous.com>
2020-01-28 14:44:52 +0800

    create 9 drum pads

--- src/index.js
+++ src/index.js
@@ -21,7 +21,37 @@ class Display extends React.Component {
 
 class DrumPad extends React.Component {
   render() {
-    return <div id="pad-bank">Pads</div>;
+    return (
+      <div id="pad-bank">
+        <div id="key1" className="drum-pad">
+          <p>Q</p>
+        </div>
+        <div id="key2" className="drum-pad">
+          <p>W</p>
+        </div>
+        <div id="key3" className="drum-pad">
+          <p>E</p>
+        </div>
+        <div id="key4" className="drum-pad">
+          <p>A</p>
+        </div>
+        <div id="key5" className="drum-pad">
+          <p>S</p>
+        </div>
+        <div id="key6" className="drum-pad">
+          <p>D</p>
+        </div>
+        <div id="key7" className="drum-pad">
+          <p>Z</p>
+        </div>
+        <div id="key8" className="drum-pad">
+          <p>X</p>
+        </div>
+        <div id="key9" className="drum-pad">
+          <p>C</p>
+        </div>
+      </div>
+    );
   }
 }
 

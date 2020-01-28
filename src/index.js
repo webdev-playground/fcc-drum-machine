28760adf558ba72aa6f068748964f669d99035cb
Josh Choo <dev@joshuous.com>
2020-01-28 14:41:52 +0800

    create App, Display, and DrumPad skeleton components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="drum-machine">
+        <Display />
+        <DrumPad />
+      </div>
+    );
+  }
+}
+
+class Display extends React.Component {
+  render() {
+    return <div id="display">Display</div>;
+  }
+}
+
+class DrumPad extends React.Component {
+  render() {
+    return <div id="pad-bank">Pads</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
